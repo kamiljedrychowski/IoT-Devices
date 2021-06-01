6d6b1f6f606c19257de2157ab8334e942cb08e85
Kamil Jędrychowski <kamil1998521@gmail.com>
2021-06-01 22:55:16 +0200

    Initial commit

---
+++ src/main/java/com/iot/devices/IoTDevicesApplication.java
@@ -0,0 +1,13 @@
+package com.iot.devices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTDevicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoTDevicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iot/devices/IoTDevicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iot.devices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTDevicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
